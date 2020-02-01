990fa29a7c6fa09c373b001d0e58556b2c35b8dd
KerrLiu <KerrForLiu@gmail.com>
2020-02-01 11:32:04 +0800

    调整key数组布局

--- config.h
+++ config.h
@@ -90,82 +90,68 @@ static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34
 #include <X11/XF86keysym.h>
 #include "shiftview.c"
 static Key keys[] = {
-	/* modifier                     key        function        argument */
+	/* modifier					key				function		argument */
 	STACKKEYS(MODKEY,                          focus)
 	STACKKEYS(MODKEY|ShiftMask,                push)
-	/* { MODKEY|ShiftMask,		XK_grave,	togglescratch,	SHCMD("") }, */
-	TAGKEYS(			XK_1,		0)
-	TAGKEYS(			XK_2,		1)
-	TAGKEYS(			XK_3,		2)
-	TAGKEYS(			XK_4,		3)
-	TAGKEYS(			XK_5,		4)
-	TAGKEYS(			XK_6,		5)
-	TAGKEYS(			XK_7,		6)
-	TAGKEYS(			XK_8,		7)
-	TAGKEYS(			XK_9,		8)
-	{ MODKEY,			XK_0,		view,		{.ui = ~0 } },
-	{ MODKEY|ShiftMask,		XK_0,		tag,		{.ui = ~0 } },
-	{ MODKEY,			XK_q,		killclient,	{0} },
-	{ MODKEY,			XK_t,		setlayout,	{.v = &layouts[0]} },
-	{ MODKEY,			XK_u,		setlayout,	{.v = &layouts[3]} },
-	{ MODKEY,			XK_i,		incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask,		XK_i,		incnmaster,     {.i = -1 } },
-	{ MODKEY,			XK_o,		setlayout,	{.v = &layouts[4]} },
-	{ MODKEY,			XK_backslash,		view,		{0} },
-	{ MODKEY,                       XK_s,      togglesticky,   {0} },
-	{ MODKEY|ShiftMask,		XK_d,		togglegaps,	{0} },
-	{ MODKEY,			XK_f,		togglefullscr,	{0} },
-	/* { MODKEY|ShiftMask,		XK_f,		togglefullscr,	{0} }, */
-	{ MODKEY,			XK_g,		shiftview,	{ .i = -1 } },
-	{ MODKEY,			XK_h,		setmfact,	{.f = -0.05} },
-	{ MODKEY,			XK_l,		setmfact,      	{.f = +0.05} },
-	{ MODKEY,			XK_semicolon,	shiftview,	{ .i = 1 } },
-	{ MODKEY,			XK_Return,	spawn,		{.v = termcmd } },
-	/*{ MODKEY|ShiftMask,		XK_Return,	togglescratch,	{.v = scratchpadcmd } },*/
-
-	{ MODKEY,			XK_z,		incrgaps,	{.i = +1 } },
-	{ MODKEY|ShiftMask,		XK_z,		incrgaps,	{.i = -1 } },
-	/* { MODKEY|ShiftMask,		XK_v,		spawn,		SHCMD("") }, */
-	{ MODKEY,			XK_b,		togglebar,	{0} },
-	/* { MODKEY|ShiftMask,		XK_period,	spawn,		SHCMD("") }, */
-
-
-	{ MODKEY,			XK_Page_Up,	shiftview,	{ .i = -1 } },
-	{ MODKEY,			XK_Page_Down,	shiftview,	{ .i = 1 } },
-	{ MODKEY,			XK_Insert,		spawn,		SHCMD("showclip") },
-
-	{ MODKEY|ShiftMask,	XK_e,		quit,		{0} },
-	{ MODKEY,			XK_F5,		xrdb,		{.v = NULL } },
-	/* { MODKEY,			XK_F11,		spawn,		SHCMD("") }, */
-
-	/* { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, */
-
-
-	{ MODKEY,			XK_space,	zoom,		{0} },
-	{ MODKEY|ShiftMask,		XK_space,	togglefloating,	{0} },
-
-	/* { MODKEY,                       XK_space,  setlayout,      {0} }, */
-
-	/* { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } }, */
-	/* { MODKEY,                       XK_period, focusmon,       {.i = +1 } }, */
-	/* { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } }, */
-	/* { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } }, */
-
-	/* { MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} }, */
-	/* { MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } }, */
-	/* { MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } }, */
-	/* { MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } }, */
-	/* { MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } }, */
-	/* { MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } }, */
-	/* { MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } }, */
+	TAGKEYS(					XK_1,		0)
+	TAGKEYS(					XK_2,		1)
+	TAGKEYS(					XK_3,		2)
+	TAGKEYS(					XK_4,		3)
+	TAGKEYS(					XK_5,		4)
+	TAGKEYS(					XK_6,		5)
+	TAGKEYS(					XK_7,		6)
+	TAGKEYS(					XK_8,		7)
+	TAGKEYS(					XK_9,		8)
+	{ MODKEY,					XK_0,			view,			{.ui = ~0 } },
+	{ MODKEY|ShiftMask,			XK_0,			tag,			{.ui = ~0 } },
+	{ MODKEY,					XK_q,			killclient,		{0} },
+	{ MODKEY,					XK_t,			setlayout,		{.v = &layouts[0]} },
+	{ MODKEY,					XK_u,			setlayout,		{.v = &layouts[3]} },
+	{ MODKEY,					XK_i,			incnmaster,     {.i = +1 } },
+	{ MODKEY|ShiftMask,			XK_i,			incnmaster,     {.i = -1 } },
+	{ MODKEY,					XK_o,			setlayout,		{.v = &layouts[4]} },
+	/* { MODKEY,				XK_backslash,	view,			{0} }, */
+	{ MODKEY,					XK_Tab,			view,			{0} },
+	{ MODKEY,					XK_s,			togglesticky,   {0} },
+	{ MODKEY|ShiftMask,			XK_d,			togglegaps,		{0} },
+	{ MODKEY,					XK_f,			togglefullscr,	{0} },
+	/* { MODKEY|ShiftMask,		XK_f,			togglefullscr,	{0} }, */
+	{ MODKEY,					XK_g,			shiftview,		{.i = -1 } },
+	{ MODKEY,					XK_h,			setmfact,		{.f = -0.05} },
+	{ MODKEY,					XK_l,			setmfact,      	{.f = +0.05} },
+	{ MODKEY,					XK_semicolon,	shiftview,		{.i = 1 } },
+	{ MODKEY,					XK_Return,		spawn,			{.v = termcmd } },
+	/*{ MODKEY|ShiftMask,		XK_Return,		togglescratch,	{.v = scratchpadcmd } },*/
+	{ MODKEY,					XK_z,			incrgaps,		{.i = +1 } },
+	{ MODKEY|ShiftMask,			XK_z,			incrgaps,		{.i = -1 } },
+	{ MODKEY,					XK_b,			togglebar,		{0} },
+	{ MODKEY,					XK_Page_Up,		shiftview,		{.i = -1 } },
+	{ MODKEY,					XK_Page_Down,	shiftview,		{.i = 1 } },
+	{ MODKEY|ShiftMask,			XK_e,			quit,			{0} },
+	{ MODKEY,					XK_F5,			xrdb,			{.v = NULL } },
+	/* { MODKEY,				XK_m,			setlayout,      {.v = &layouts[2]} }, */
+	{ MODKEY,					XK_space,		zoom,			{0} },
+	{ MODKEY|ShiftMask,			XK_space,		togglefloating,	{0} },
+	/* { MODKEY,				XK_space,  setlayout,			{0} }, */
+	/* { MODKEY,				XK_comma,  focusmon,			{.i = -1 } }, */
+	/* { MODKEY,				XK_period, focusmon,			{.i = +1 } }, */
+	/* { MODKEY|ShiftMask,		XK_comma,  tagmon,				{.i = -1 } }, */
+	/* { MODKEY|ShiftMask,		XK_period, tagmon,				{.i = +1 } }, */
+	/* { MODKEY|Mod4Mask,              XK_h,      incrgaps,		{.i = +1 } }, */
+	/* { MODKEY|Mod4Mask,              XK_l,      incrgaps,		{.i = -1 } }, */
+	/* { MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,	{.i = +1 } }, */
+	/* { MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,	{.i = -1 } }, */
+	/* { MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,	{.i = +1 } }, */
+	/* { MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,	{.i = -1 } }, */
+	/* { MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,	{0} }, */
+	/* { MODKEY,                       XK_y,      incrihgaps,	{.i = +1 } }, */
+	/* { MODKEY,                       XK_o,      incrihgaps,	{.i = -1 } }, */
+	/* { MODKEY|ControlMask,           XK_y,      incrivgaps,	{.i = +1 } }, */
+	/* { MODKEY|ControlMask,           XK_o,      incrivgaps,	{.i = -1 } }, */
+	/* { MODKEY|Mod4Mask,              XK_y,      incrohgaps,	{.i = +1 } }, */
+	/* { MODKEY|Mod4Mask,              XK_o,      incrohgaps,	{.i = -1 } }, */
+	/* { MODKEY|ShiftMask,             XK_y,      incrovgaps,	{.i = +1 } }, */
+	/* { MODKEY|ShiftMask,             XK_o,      incrovgaps,	{.i = -1 } }, */
 
 
 };
